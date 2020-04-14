fe97203b7bcb2a8d1495d140e4ddd539451df9a8
Hwajam <hwajam000@gmail.com>
2020-04-14 18:00:52 +0900

    player生成

---
+++ Csharp_TextRPG/Player.cs
@@ -0,0 +1,82 @@
+namespace Csharp
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+    class Player
+    {
+        protected PlayerType type = PlayerType.None;
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Player(PlayerType type)
+        {
+            this.type = type;
+        }
+        
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public PlayerType GetPlayerType()
+        {
+            return type;
+        }
+        
+        public int GetHp()
+        {
+            return hp; 
+        }
+
+        public int GetAttack()
+        {
+            return attack;
+        }
+
+        public bool IsDead()
+        {
+            return hp <= 0;
+        }
+
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+        }
+    }
+
+    class Knight : Player
+    {
+        //생성자 (생성자는 반환 형식이 없음)
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+    
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+    
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+    
+}
